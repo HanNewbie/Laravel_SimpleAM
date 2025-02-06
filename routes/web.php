b5669c68a01b2e1394eaa50cd4d6dfdc9ec0e955
Farhan <146110996+HanNewbie@users.noreply.github.com>
2025-02-06 09:14:24 +0700

    Code

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ use App\Http\Controllers\DashboardController;
 |
 */
 
+//DASHBOARD
 Route::get('/', [DashboardController::class, 'index']);
 
 //ROUTE CUSTOMER
@@ -33,6 +34,7 @@ Route::get('/customer/{NoBilling}/edit', [CustomerController::class, 'edit'])->n
 Route::put('/customer/{NoBilling}', [CustomerController::class, 'update'])->name('admin.customer.update');
 Route::delete('/customer/{NoBilling}', [CustomerController::class, 'destroy'])->name('admin.customer.destroy');
 
+//ROUTE MANAGER
 Route::get('/manager', [ManagerController::class, 'index'])->name('admin.manager.index');
 Route::get('/manager/create', [ManagerController::class, 'create'])->name('admin.manager.create');
 Route::post('/manager', [ManagerController::class, 'store'])->name('admin.manager.store');
@@ -40,6 +42,7 @@ Route::get('/manager/{NIKAM}/edit', [ManagerController::class, 'edit'])->name('a
 Route::put('/manager/{NIKAM}', [ManagerController::class, 'update'])->name('admin.manager.update');
 Route::delete('/manager/{NIKAM}', [ManagerController::class, 'destroy'])->name('admin.manager.destroy');
 
+//ROUTE LAYANAN
 Route::get('/layanan', [LayananController::class, 'index'])->name('admin.layanan.index');
 Route::get('/layanan/create', [LayananController::class, 'create'])->name('admin.layanan.create');
 Route::post('/layanan', [LayananController::class, 'store'])->name('admin.layanan.store');
@@ -47,6 +50,7 @@ Route::get('/layanan/{SID}/edit', [LayananController::class, 'edit'])->name('adm
 Route::put('/layanan/{SID}', [LayananController::class, 'update'])->name('admin.layanan.update');
 Route::delete('/layanan/{SID}', [LayananController::class, 'destroy'])->name('admin.layanan.destroy');
 
+//ROUTE KONTRAK
 Route::get('/kontrak', [KontrakController::class, 'index'])->name('admin.kontrak.index');
 Route::get('/kontrak/create', [KontrakController::class, 'create'])->name('admin.kontrak.create');
 Route::post('/kontrak', [KontrakController::class, 'store'])->name('admin.kontrak.store');
